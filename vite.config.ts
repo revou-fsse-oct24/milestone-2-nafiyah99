f81d764c096e3853e3c77ae59e843e18ae278cd1
nafiyah99 <nafiyah99@gmail.com>
2025-01-13 21:03:06 +0700

    still fixing some issue: deployment issue

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import tailwindcss from "tailwindcss"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+  },
   css: {
     postcss: {
       plugins: [tailwindcss()],
